f60732a6bc4ae75baeef23d74df1b4969c54686a
Brett K <bkapla01@gmail.com>
2024-01-18 22:43:57 -0600

    Logic for FrontEnd Authentication

--- web/frontend/src/pages/SignIn.js
+++ web/frontend/src/pages/SignIn.js
@@ -3,32 +3,101 @@ import * as React from "react";
 import logo from '../logo.svg';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEye, faSignIn, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faEye, faEyeSlash, faSignIn, faUser } from '@fortawesome/free-solid-svg-icons'
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function SignInPage(props) {
 
-    const [email, setEmail] = React.useState("");
-    const [password, setPassword] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [passwordType, setPasswordType] = React.useState("password");
 
-    const submitLogin = async () => {
-	const hash = btoa(email + ":" + password);
-        const headers = { 'Authorization': `Basic ${hash}` };
-	
-	const res = await fetch("./login", { method: "POST", headers });
-	console.log(res);
+  const [errorText, setErrorText] = React.useState("");
+
+  const validateInfo = (email) => {
+    if (email !== "" && (!email.includes("@") || !email.includes(".") || email.split("@").pop().length < 2 || email.split(".").pop().length < 2)) {
+      setErrorText("Invalid email.");
+      return false;
     }
-    
+    setErrorText("");
+    return true;
+  }
+
+  const canLogin = () => {
+    if (!validateInfo(email)) return false;
+
+    if (email === "") {
+      setErrorText("Email cannot be empty.");
+      return false;
+    }
+
+    if (password === "") {
+      setErrorText("Password cannot be empty.");
+      return false;
+    }
+
+    return true;
+  }
+
+  const submitLogin = async () => {
+    if (!canLogin()) return;
+
+    const hash = btoa(email + ":" + password);
+    const headers = { 'Authorization': `Basic ${hash}` };
+
+    try {
+      const res = await fetch("./login", { method: "POST", headers });
+      console.log(res);
+
+      if (res.status === 200) {
+        toast.success("Successfully logged in.", {
+          position: "top-right",
+          autoClose: 2000,
+          closeOnClick: true,
+          pauseOnHover: true,
+          theme: "light"
+        });
+        setTimeout(() => {
+          window.location.href = process.env.PUBLIC_URL + "/dashboard";
+        }, 250);
+      } else {
+        toast.error(res.status === 401 ? "Invalid Credentials." : res.statusText, {
+          position: "top-right",
+          autoClose: 2000,
+          closeOnClick: true,
+          pauseOnHover: true,
+          theme: "light"
+        });
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   return (
-      <form
-	  action="./login"
-	  method="/post"
-	  className="justify-center items-center bg-neutral-200 min-h-[100vh] flex flex-col px-16 py-11 max-md:px-5"
-      >
-      <span className="shadow-sm bg-blue-950 flex w-[650px] max-w-full flex-col mb-5 pl-12 pr-8 pb-2.5 rounded-2xl max-md:px-5 text-white">
+    <form
+      action="./login"
+      method="/post"
+      className="justify-center items-center bg-neutral-200 min-h-[100vh] flex flex-col px-16 py-11 max-md:px-5"
+    >
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnFocusLoss
+        pauseOnHover
+        theme="light"
+      />
+
+      <span className="shadow-lg bg-blue-950 flex w-[650px] max-w-full flex-col mb-5 pl-12 pr-8 pb-2.5 rounded-2xl max-md:px-5 text-white">
         <img
-            src={logo}
-            alt="Eagle Engagement Logo"
-            className="h-40 mb-[-80px]"
+          src={logo}
+          alt="Eagle Engagement Logo"
+          className="h-40 mb-[-80px]"
         />
         <div className="justify-center items-center self-stretch flex flex-col mt-11 px-16 max-md:max-w-full max-md:mt-10 max-md:px-5">
           <div className="flex max-w-full flex-col items-center">
@@ -37,51 +106,64 @@ function SignInPage(props) {
               Welcome!
             </span>
             <span className="text-xl max-md:mb-4 max-md:text-l">
-                Sign into your account
+              Sign into your account
             </span>
           </div>
         </div>
         <div>
-            <span className="text-2xl max-md:text-xl">Email</span>
-            <div class="flex items-center border-b border-white py-2 px-4 text-xl max-md:text-l">
-                <input
-		    className="appearance-none bg-transparent border-none w-full placeholder-white text-gray-100 mr-3 py-1 leading-tight focus:outline-none"
-		    type="email"
-		    placeholder=" "
-		    aria-label="Email"
-		    value={email}
-		    onChange={(e) => setEmail(e.target.value)}
-		/>
-                <FontAwesomeIcon icon={faUser} size="m" />
-            </div>
+          <span className="text-2xl max-md:text-xl">Email</span>
+          <div className="flex items-center border-b border-white py-2 px-4 text-xl max-md:text-l">
+            <input
+              className="appearance-none bg-transparent border-none w-full placeholder-white text-gray-100 mr-3 py-1 leading-tight focus:outline-none"
+              type="email"
+              placeholder=" "
+              aria-label="Email"
+              value={email}
+              onChange={(e) => { setEmail(e.target.value); validateInfo(e.target.value) }}
+            />
+            <FontAwesomeIcon icon={faUser} size="sm" />
+          </div>
         </div>
         <div className="mt-5">
-            <span className="text-2xl max-md:text-xl">Password</span>
-            <div class="flex items-center border-b border-white py-2 px-4 text-xl max-md:text-l">
-                <input
-		    className="appearance-none bg-transparent border-none w-full placeholder-white text-gray-100 mr-3 py-1 leading-tight focus:outline-none"
-		    type="password"
-		    placeholder=""
-		    aria-label="Password"
-		    value={password}
-		    onChange={(e) => setPassword(e.target.value)}
-		/>
-                <button id="togglePassword">
-                    <FontAwesomeIcon icon={faEye} size="m" />
-                </button>
-            </div>
+          <span className="text-2xl max-md:text-xl">Password</span>
+          <div className="flex items-center border-b border-white py-2 px-4 text-xl max-md:text-l">
+            <input
+              className="appearance-none bg-transparent border-none w-full placeholder-white text-gray-100 mr-3 py-1 leading-tight focus:outline-none"
+              type={passwordType}
+              placeholder=""
+              aria-label="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button
+              id="togglePassword"
+              type="button"
+              onClick={() => {
+                setPasswordType(passwordType === "password" ? "text" : "password");
+              }}
+            >
+              <FontAwesomeIcon
+                icon={passwordType === "password" ? faEyeSlash : faEye}
+                size="sm"
+                style={{ width: "25px" }}
+              />
+            </button>
+          </div>
         </div>
-          <button
-	      onClick={submitLogin}
-	      type="button"
-	      className="text-center text-3xl font-semibold whitespace-nowrap justify-center items-center bg-blue-900 self-center w-[50%] max-w-full mt-11 px-16 py-2 rounded-xl max-md:text-2xl max-md:mt-10 max-md:px-5"
-	  >
+        <span className={`text-red-500 text-center mt-5 ${errorText ? "opacity-100" : "opacity-0"}`}>
+          {errorText ? `*${errorText}` : "|"}
+        </span>
+        <button
+          onClick={submitLogin}
+          type="button"
+          className="text-center text-3xl font-semibold whitespace-nowrap justify-center items-center bg-blue-900 self-center w-[50%] max-w-full mt-11 px-16 py-2 rounded-xl max-md:text-2xl max-md:mt-10 max-md:px-5"
+        >
           Login
         </button>
         <div className="text-center text-2xl self-center mt-11 leading-20 max-md:text-xl max-md:mt-10">
-            <span>Don't Have an Account? <a className="underline" href={process.env.PUBLIC_URL + "/signup"}>Click</a></span>
+          <span>Don't Have an Account? <a className="underline" href={process.env.PUBLIC_URL + "/signup"}>Click</a></span>
           <br />
-            <span>Forgot Your Password? <a className="underline" href={process.env.PUBLIC_URL + "/forgotpassword"}>Click</a></span>
+          <span>Forgot Your Password? <a className="underline" href={process.env.PUBLIC_URL + "/forgotpassword"}>Click</a></span>
         </div>
       </span>
     </form>
--- web/frontend/src/pages/SignUp.js
+++ web/frontend/src/pages/SignUp.js
@@ -3,16 +3,73 @@ import * as React from "react";
 import logo from '../logo.svg';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEye, faSignIn, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faEye, faEyeSlash, faSignIn, faUser } from '@fortawesome/free-solid-svg-icons'
 
 function SignInPage(props) {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [passwordConfirm, setPasswordConfirm] = React.useState("");
+  const [passwordType, setPasswordType] = React.useState("password");
+
+  const [errorText, setErrorText] = React.useState("");
+
+  /**
+   * @param {String} email
+   * @param {String} password 
+   * @param {String} passwordConfirm 
+   * @returns {Boolean}
+   */
+  const validateInfo = (email, password, passwordConfirm) => {
+    if (email !== "" && (!email.includes("@") || !email.includes(".") || email.split("@").pop().length < 2 || email.split(".").pop().length < 2)) {
+      setErrorText("Invalid email.");
+      return false;
+    }
+
+    if (passwordConfirm !== password) {
+      setErrorText("Passwords do not match.");
+      console.log(passwordConfirm, password)
+      return false;
+    }
+    setErrorText("");
+    return true;
+  }
+
+  const canSignUp = () => {
+    if (!validateInfo(email, password, passwordConfirm)) return false;
+
+    if (email === "") {
+      setErrorText("Email cannot be empty.");
+      return false;
+    }
+
+    if (password === "") {
+      setErrorText("Password cannot be empty.");
+      return false;
+    }
+
+    return true;
+  }
+
+  const submitSignUp = async () => {
+    if (!canSignUp()) return;
+
+    const body = {
+      email: email.toLowerCase(),
+      password: btoa(password),
+      passwordConfirm: btoa(passwordConfirm)
+    }
+
+    const res = await fetch("./signup", { method: "POST", body: JSON.stringify(body) });
+    console.log(res);
+  }
+
   return (
     <div className="justify-center items-center bg-neutral-200 min-h-[100vh] flex flex-col px-16 py-11 max-md:px-5">
-      <span className="shadow-sm bg-blue-950 flex w-[650px] max-w-full flex-col mb-5 pl-12 pr-8 pb-2.5 rounded-2xl max-md:px-5 text-white">
+      <span className="shadow-lg bg-blue-950 flex w-[650px] max-w-full flex-col mb-5 pl-12 pr-8 pb-2.5 rounded-2xl max-md:px-5 text-white">
         <img
-            src={logo}
-            alt="Eagle Engagement Logo"
-            className="h-40 mb-[-80px]"
+          src={logo}
+          alt="Eagle Engagement Logo"
+          className="h-40 mb-[-80px]"
         />
         <div className="justify-center items-center self-stretch flex flex-col mt-11 px-16 max-md:max-w-full max-md:mt-10 max-md:px-5">
           <div className="flex max-w-full flex-col items-center">
@@ -21,31 +78,88 @@ function SignInPage(props) {
               Welcome!
             </span>
             <span className="text-xl max-md:mb-4 max-md:text-l">
-                Create your account
+              Create your account
             </span>
           </div>
         </div>
         <div>
-            <span className="text-2xl max-md:text-xl">Email</span>
-            <div class="flex items-center border-b border-white py-2 px-4 text-xl max-md:text-l">
-                <input class="appearance-none bg-transparent border-none w-full placeholder-white text-gray-100 mr-3 py-1 leading-tight focus:outline-none" type="email" placeholder=" " aria-label="Full name" />
-                <FontAwesomeIcon icon={faUser} size="m" />
-            </div>
+          <span className="text-2xl max-md:text-xl">Email</span>
+          <div className="flex items-center border-b border-white py-2 px-4 text-xl max-md:text-l">
+            <input
+              className="appearance-none bg-transparent border-none w-full placeholder-white text-gray-100 mr-3 py-1 leading-tight focus:outline-none"
+              type="email"
+              placeholder=" "
+              aria-label="Email"
+              value={email}
+              onChange={(e) => { setEmail(e.target.value); validateInfo(e.target.value, password, passwordConfirm) }}
+            />
+            <FontAwesomeIcon icon={faUser} size="sm" />
+          </div>
         </div>
         <div className="mt-5">
-            <span className="text-2xl max-md:text-xl">Password</span>
-            <div class="flex items-center border-b border-white py-2 px-4 text-xl max-md:text-l">
-                <input class="appearance-none bg-transparent border-none w-full placeholder-white text-gray-100 mr-3 py-1 leading-tight focus:outline-none" type="password" placeholder="" aria-label="Full name" />
-                <button id="togglePassword">
-                    <FontAwesomeIcon icon={faEye} size="m" />
-                </button>
-            </div>
+          <span className="text-2xl max-md:text-xl">Password</span>
+          <div className="flex items-center border-b border-white py-2 px-4 text-xl max-md:text-l">
+            <input
+              className="appearance-none bg-transparent border-none w-full placeholder-white text-gray-100 mr-3 py-1 leading-tight focus:outline-none"
+              type={passwordType}
+              placeholder=""
+              aria-label="Password"
+              value={password}
+              onChange={(e) => { setPassword(e.target.value); validateInfo(email, e.target.value, passwordConfirm); }}
+            />
+            <button
+              id="togglePassword"
+              type="button"
+              onClick={() => {
+                setPasswordType(passwordType === "password" ? "text" : "password");
+              }}
+            >
+              <FontAwesomeIcon
+                icon={passwordType === "password" ? faEyeSlash : faEye}
+                size="sm"
+                style={{ width: "25px" }}
+              />
+            </button>
+          </div>
         </div>
-        <button className="text-center text-3xl font-semibold whitespace-nowrap justify-center items-center bg-blue-900 self-center w-[50%] max-w-full mt-11 px-16 py-2 rounded-xl max-md:text-2xl max-md:mt-10 max-md:px-5">
+        <div className="mt-5">
+          <span className="text-2xl max-md:text-xl">Confirm Password</span>
+          <div className="flex items-center border-b border-white py-2 px-4 text-xl max-md:text-l">
+            <input
+              className="appearance-none bg-transparent border-none w-full placeholder-white text-gray-100 mr-3 py-1 leading-tight focus:outline-none"
+              type={passwordType}
+              placeholder=""
+              aria-label="Confirm Password"
+              value={passwordConfirm}
+              onChange={(e) => { setPasswordConfirm(e.target.value); validateInfo(email, password, e.target.value); }}
+            />
+            <button
+              id="togglePassword"
+              type="button"
+              onClick={() => {
+                setPasswordType(passwordType === "password" ? "text" : "password");
+              }}
+            >
+              <FontAwesomeIcon
+                icon={passwordType === "password" ? faEyeSlash : faEye}
+                size="sm"
+                style={{ width: "25px" }}
+              />
+            </button>
+          </div>
+        </div>
+        <span className={`text-red-500 text-center mt-5 ${errorText ? "opacity-100" : "opacity-0"}`}>
+          {errorText ? `*${errorText}` : "|"}
+        </span>
+        <button
+          onClick={submitSignUp}
+          type="button"
+          className="text-center text-3xl font-semibold whitespace-nowrap justify-center items-center bg-blue-900 self-center w-[50%] max-w-full mt-11 px-16 py-2 rounded-xl max-md:text-2xl max-md:mt-10 max-md:px-5"
+        >
           Create
         </button>
         <div className="text-center text-2xl self-center mt-11 leading-20 max-md:text-xl max-md:mt-10">
-            <span>Already Have an Account? <a className="underline" href={process.env.PUBLIC_URL + "/login"}>Click</a></span>
+          <span>Already Have an Account? <a className="underline" href={process.env.PUBLIC_URL + "/login"}>Click</a></span>
         </div>
       </span>
     </div>
