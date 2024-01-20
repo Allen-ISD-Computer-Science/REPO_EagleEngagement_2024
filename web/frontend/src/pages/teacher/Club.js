3ff82b8e43235e22b63261ad7df877d2583006af
Brett K <bkapla01@gmail.com>
2024-01-20 17:38:47 -0600

    created club page

--- web/frontend/src/pages/teacher/Club.js
+++ web/frontend/src/pages/teacher/Club.js
@@ -1,10 +1,26 @@
 import * as React from "react";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye, faLink } from '@fortawesome/free-solid-svg-icons'
+import { faInstagram, faXTwitter, faYoutube } from "@fortawesome/free-brands-svg-icons"
+
 import TeacherNav from "../../components/TeacherNav";
-import ClubCard from "../../components/ClubCard";
 
 function ClubPage(props) {
-  const [clubObj, setClubObj] = React.useState({});
+  const [clubObj, setClubObj] = React.useState({
+    id: 1,
+    name: "Allen Eagle Robotics",
+    description: "Eagle Robotics participates in B.E.S.T. and FRC robotics competitions. We have programming, building, and business opportunities.",
+    following: 500,
+    lastCheckIns: 20,
+    link: "team5417.com",
+    twitter: "AllenRobotics",
+    instagram: "eagle.robotics",
+    youtube: "@ATeamRobotics",
+    meetingLogs: [
+      { date: "9/1/2021", numStudents: 20 },
+    ]
+  });
 
   React.useEffect(() => {
     const clubID = parseInt(window.location.pathname.split("/").pop());
@@ -32,12 +48,68 @@ function ClubPage(props) {
             Club - {clubObj.name}
           </span>
         </div>
-        
-        
+
+        <div
+          className="p-12 flex flex-col items-stretch max-md:px-5"
+        >
+          <div
+            className="flex flex-row justify-between items-stretch gap-40"
+          >
+            <div className="flex flex-col">
+              <img
+                srcSet={process.env.PUBLIC_URL + "/clubLogos/" + clubObj.id}
+                alt={clubObj.name + " Logo"}
+                className="aspect-square object-contain object-center w-[185px] mx-3 overflow-hidden shrink-0 max-w-full rounded-[50%] max-md:mt-10"
+              />
+              <span className="text-xl">{clubObj.following + " Students Following"}</span>
+            </div>
+            <div className="flex flex-col leading-10">
+              <span className="text-4xl font-bold my-6">{clubObj.name}</span>
+              <span className="text-2xl h-[96px] pr-10">{clubObj.description}</span>
+              <div className="flex flex-row gap-5 text-xl">
+                {clubObj.link ? <a href={"https://" + clubObj.link} target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLink} size="s" /> {clubObj.link}</a> : ""}
+                {clubObj.twitter ? <a href={"https://x.com/" + clubObj.twitter} target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faXTwitter} size="s" /> @{clubObj.twitter}</a> : ""}
+                {clubObj.instagram ? <a  href={"https://instagram.com/" + clubObj.instagram} target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faInstagram} size="s" /> @{clubObj.instagram}</a> : ""}
+                {clubObj.youtube ? <a href={"https://youtube.com/" + clubObj.youtube} target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faYoutube} size="s" /> {clubObj.youtube}</a> : ""}
+              </div>
+            </div>
+
+          </div>
+
+          <div className="flex flex-col mt-10">
+            <h3 className="text-3xl font-bold text-center">Meeting Logs</h3>
+            <table
+              className="bg-gray-100 mt-8 table-fixed text-center w-full border border-slate-600 [&_th]:border-slate-600 [&_td]:border-slate-600 [&_th]:border-r [&_td]:border-r [&_th]:p-2 [&_td]:p-2"
+              style={{ borderRadius: "1rem !important" }}
+            >
+              <thead className="border-b border-slate-600">
+                {/* date, number of students, and actions columns */}
+                <tr className="text-xl font-bold">
+                  <th>Date</th>
+                  <th>Number of Students</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody className="border-b border-slate-600">
+                {/* meeting logs */}
+                {
+                  clubObj.meetingLogs.map((meetingLog, i) =>
+                      <tr key={i} className="text-l">
+                        <td>{meetingLog.date}</td>
+                        <td>{meetingLog.numStudents}</td>
+                        <td>
+                          <button className="bg-blue-950 text-white px-4 py-2 rounded-xl"><FontAwesomeIcon icon={faEye} size="m" /></button>
+                        </td>
+                      </tr>
+                  )
+                }
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
-
 
 export default ClubPage;
