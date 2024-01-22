c837b6bf6d402236a74ecde88e43e99b9f68bd08
LayneY <layne.yarbrough@student.allenisd.org>
2024-01-22 14:16:21 -0600

    Finished importing pages from #37 Started navigation stuff from #32

--- android/app/src/main/java/com/ahscs/eagleengagement/MainActivity.kt
+++ android/app/src/main/java/com/ahscs/eagleengagement/MainActivity.kt
@@ -2,10 +2,31 @@ package com.ahscs.eagleengagement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+   lateinit var bottomNav : BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadFragment(EventsFragment())
+        bottomNav = findViewById(R.id.navBar) as BottomNavigationView
+//        bottomNav.setOnItemSelectedListener {
+//            when(it.itemId) {
+//                R.id.clubs -> {
+//                    loadFragment(ClubsFragment)
+//                    true
+//                }
+//            }
+//        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.commit()
     }
 }
