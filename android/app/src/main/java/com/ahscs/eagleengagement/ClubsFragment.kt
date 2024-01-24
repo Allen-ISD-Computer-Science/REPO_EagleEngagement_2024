a69faae90ebb83f7fc466a552c6db0892dd4c699
LayneY <layne.yarbrough@student.allenisd.org>
2024-01-23 19:07:53 -0600

    finished main page (home, clubs, and rewards) navigation for #32 other pages not done yet, also started working on recycler view for events

--- android/app/src/main/java/com/ahscs/eagleengagement/ClubsFragment.kt
+++ android/app/src/main/java/com/ahscs/eagleengagement/ClubsFragment.kt
@@ -6,14 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class EventsFragment : Fragment() {
+class ClubsFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_page, container, false)
+        return inflater.inflate(R.layout.fragment_clubs_page, container, false)
     }
 
 }
