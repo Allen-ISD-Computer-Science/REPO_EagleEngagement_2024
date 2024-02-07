4a7d9556f197de5659ec6f5c49d8b367ea2fcb4c
Brett Kaplan <bkapla01@gmail.com>
2024-02-07 13:06:06 -0600

    Started Email sending through API for #57

--- web/backend/Sources/EagleEngagement/App/Authenticators/TeacherSignUp.swift
+++ web/backend/Sources/EagleEngagement/App/Authenticators/TeacherSignUp.swift
@@ -6,13 +6,11 @@ struct TeacherSignUpParams : Content {
     var firstName : String;
     var lastName : String;
     var email : String;
-    var password : String;
-    var passwordConfirm: String;
 }
 
 struct TeacherSignUp {
     func randomString(length: Int) -> String {
-        let letters = "abcdefgh0123456789"
+        let letters = "0123456789"
         return String((0..<length).map{ _ in letters.randomElement()! })
     }
 
@@ -47,10 +45,48 @@ struct TeacherSignUp {
 
         try await user.save(on: req.db);
 
+        guard let facultySignUpTemplateID = Environment.get("FACULTY_SIGNUP_TEMPLATE_ID") else {
+            fatalError("Failed to determine FACULTY_SIGNUP_TEMPLATE_ID from environment");
+        }
+
+        let contact = EmailContact(firstName: args.firstName, lastName: args.lastName, emailAddress: user.email)
+        let verifyEmail = TokenURLEmail(pathComponet: "verify", token: verificationString).createContent(with: contact)
+        let emailData = EmailData(contact: contact,
+                                  templateExternalID: facultySignUpTemplateID,
+                                  templateParameters: verifyEmail)
+        
+        try await GlobalEmailAPI.sendEmail(from: req, with: emailData)
+
         print("Created: \(args.email) with verificationToken: \(verificationString)");
         
         return Msg(success: true, msg: "Created User! Check email for verification code!");
+    }
 
+    struct TokenURLEmail: EmailContactConsumer {
+        private struct TokenURLEmailWrapper: Encodable {
+            let firstName: String?
+            let lastName: String?
+            let tokenURL: URL
+        }
+
+        let tokenURL: URL
+
+        init(pathComponet: String, token: String) {
+            guard let vaporPublic = Environment.get("VAPOR_SERVER_PUBLIC_URL"), let vaporPublicURL = URL(string: vaporPublic) else {
+                fatalError("Failed to determine VAPOR_SERVER_PUBIC_URL from environment");
+            }
+                    
+            self.tokenURL = vaporPublicURL
+              .appendingPathComponent(pathComponet)
+              .appendingPathComponent(token)
+        }
+
+        func createContent(with contact: EmailContact) -> Encodable {
+            return TokenURLEmailWrapper(firstName: contact.firstName,
+                                        lastName: contact.lastName,
+                                        tokenURL: self.tokenURL
+            )
+        }
     }
     
 }
