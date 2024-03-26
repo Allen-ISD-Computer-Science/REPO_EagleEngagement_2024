650dc05ab9eb4d4260fdfdd7efb0b752fcb67ae1
Logan-Rohlfs <loganrohlfs76@gmail.com>
2024-03-26 14:59:47 -0500

    Fix - Adds comments to various files

--- ios/ConnectEDU/ConnectEDU/Models/Club.swift
+++ ios/ConnectEDU/ConnectEDU/Models/Club.swift
@@ -8,15 +8,15 @@
 import Foundation
 
 struct Club: Codable {
-    var name: String
-    var description: String
-    var sponsors: String?
-    var meetingTimes: String?
-    var locationName: String?
-    var websiteLink: String?
-    var instagramLink: String?
-    var twitterLink: String?
-    var youtubeLink: String?
+    var name: String // Name of the club
+    var description: String // Descritpion of the club
+    var sponsors: String? // Sponsors of the Club
+    var meetingTimes: String? // Meeting times in the string format
+    var locationName: String? // Location Name (not the same as address)
+    var websiteLink: String? // Links to the clubs website (if it exists)
+    var instagramLink: String? // Same as above
+    var twitterLink: String? // Same as above
+    var youtubeLink: String? // Same as above
 }
 
 //POST /api/club/:id
--- ios/ConnectEDU/ConnectEDU/ViewModels/Authentication/LoginViewModel.swift
+++ ios/ConnectEDU/ConnectEDU/ViewModels/Authentication/LoginViewModel.swift
@@ -8,18 +8,22 @@
 import Foundation
 
 class LoginViewModel: ObservableObject {
+    // Initializes the navigation manager for the login page to later be inputted
     private var loginNavigationManager: NavigationManager
 
+    // User Input Fields
     @Published var email: String = ""
     @Published var password: String = ""
     @Published var loginMessage: String = ""
 
+    // Stores the Auth Result for displaying the correct "color" of resulting message
     var loginAuthResult: Bool = false
 
     init(navigationManager: NavigationManager) {
         self.loginNavigationManager = navigationManager
     }
 
+    // Attempts a user login API request and handles the response properly
     func login() {
         APIService.login(email: email, password: password) { result, message in
             DispatchQueue.main.async {
